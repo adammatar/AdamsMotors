3aa677903efe4c3375cc8f0d229083c7e00497b0
adammatar <mataradam@gmail.com>
2024-05-13 19:40:27 +0300

    Update GetCode.tsx

--- adams-motors/components/GetCode.tsx
+++ adams-motors/components/GetCode.tsx
@@ -31,7 +31,7 @@ const GetCode = () => {
             </p>
             <div className="mt-10 flex items-center justify-center lg:justify-start">
                 <a
-                  href="#"
+                  href="https://github.com/adammatar/AdamsMotors"
                   className=" rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white w-full"
                 >
                   <div className="flex items-center justify-center flex-shrink-0">
